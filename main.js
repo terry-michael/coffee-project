16ccfbb69350f969ddbd1f3174775a9d6cd68752
terryhale <aggiefan17@yahoo.com>
2017-10-26 10:02:18 -0500

    Added some bootstrap and search bar

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function updateCoffees(e) {
             filteredCoffees.push(coffee);
         }
     });
-    coffeeDiv.innerHTML = renderCoffees(filteredCoffees);
+    tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
@@ -48,10 +48,13 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'}
 ];
 
-var coffeeDiv = document.querySelector('#coffees');
+var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
-coffeeDiv.innerHTML = renderCoffees(coffees);
+tbody.innerHTML = renderCoffees(coffees);
 
 submitButton.addEventListener('click', updateCoffees);
+
+
+
